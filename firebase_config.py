51a9201e06c139397d4ccce4048f0bd24fbd004d
Pooja Srivastava_data <sri.pooja1194@gmail.com>
2026-08-18 21:28:24 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,23 +1,12 @@
+import json
 import streamlit as st
 from firebase_admin import credentials, initialize_app, firestore
 
 if not st.session_state.get('firebase_initialized', False):
-    # Hum directly secrets ki values ko dictionary mein bhaar rahe hain
-    cred_dict = {
-        "type": st.secrets["firebase"]["type"],
-        "project_id": st.secrets["firebase"]["project_id"],
-        "private_key_id": st.secrets["firebase"]["private_key_id"],
-        "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
-        "client_email": st.secrets["firebase"]["client_email"],
-        "client_id": st.secrets["firebase"]["client_id"],
-        "auth_uri": st.secrets["firebase"]["auth_uri"],
-        "token_uri": st.secrets["firebase"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
-        "universe_domain": st.secrets["firebase"]["universe_domain"]
-    }
-    
-    cred = credentials.Certificate(cred_dict)
+    # Secrets se direct raw JSON string uthakar parse kar rahe hain
+    key_dict = json.loads(st.secrets["FIREBASE_JSON"])
+
+    cred = credentials.Certificate(key_dict)
     initialize_app(cred)
     st.session_state['firebase_initialized'] = True
 

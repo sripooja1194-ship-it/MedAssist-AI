2152b7d56944d23eb78b8559ee89bcaa6cf1c375
Pooja Srivastava_data <sri.pooja1194@gmail.com>
2026-08-18 21:18:39 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,11 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
+import streamlit as st
+from firebase_admin import credentials, initialize_app, firestore
 
-# Initialize Firebase only once
-if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
-    firebase_admin.initialize_app(cred)
-
-print("✅ Firebase Connected Successfully!")
+# Check karo agar app pehle se initialized nahi hai toh initialize karo
+if not st.session_state.get('firebase_initialized', False):
+    firebase_creds = dict(st.secrets["firebase"])
+    cred = credentials.Certificate(firebase_creds)
+    initialize_app(cred)
+    st.session_state['firebase_initialized'] = True
 
 db = firestore.client()
